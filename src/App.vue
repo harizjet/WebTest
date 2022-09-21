a7b748d8e7cb74263614504d15ba6bf4aeb4fd04
Hariz Izzat <harizizzat09@gmail.com>
2022-09-21 23:29:25 +0800

    added update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ todos }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    components: {},
+    data() {
+      return {
+        todos: [
+          {
+            id: 1,
+            title: "todo one",
+            completed: false,
+          },
+          {
+            id: 2,
+            title: "todo two",
+            completed: true,
+          },
+          {
+            id: 3,
+            title: "todo third",
+            completed: false,
+          },
+        ],
+      };
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
